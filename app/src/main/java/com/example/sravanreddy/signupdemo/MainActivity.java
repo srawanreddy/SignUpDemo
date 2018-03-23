6af0273695937dd8ae6898528dbe5e32ebd96749
srawan reddy <srawanreddy@gmail.com>
2018-03-23 14:07:36 -0500

    classwork1

--- app/src/main/java/com/example/sravanreddy/signupdemo/MainActivity.java
+++ app/src/main/java/com/example/sravanreddy/signupdemo/MainActivity.java
@@ -1,28 +1,60 @@
 package com.example.sravanreddy.signupdemo;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.spinner_main); //socket
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, R.layout.spinner_layout,R.id.spinnerText, languages);
+        spinner.setAdapter(arrayAdapter);
     }
-
+    String[] languages = {"hindi","english","chinese","urdu"};
     public void clickHandler(View view) {
         switch(view.getId()){
             case R.id.signupButton:
                 Intent signupIntent=new Intent(MainActivity.this, SignUp.class);
                 startActivity(signupIntent);
                 break;
-            case R.id.userNameEditText:
-
+            case R.id.loginButton:
+                showDialog();
                 break;
         }
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder alertDialog=new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("No Action");
+        alertDialog.setMessage("No action is defined for this button");
+        alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alertDialog.show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
--- app/src/main/java/com/example/sravanreddy/signupdemo/SignUp.java
+++ app/src/main/java/com/example/sravanreddy/signupdemo/SignUp.java
@@ -6,50 +6,54 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity implements View.OnFocusChangeListener {
     EditText firstName;
     EditText middleName;
     EditText lastName;
     EditText emailId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        firstName = (EditText) findViewById(R.id.firstNameEditText);
+        middleName=(EditText)findViewById(R.id.middlename_EditText);
+        lastName=(EditText)findViewById(R.id.lastname_EditText);
+        emailId = findViewById(R.id.email_EditText);
+        firstName.setOnFocusChangeListener(this);
 
     }
 
     public void signupClickHadler(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.back_Button:
-                Intent backIntent=new Intent(SignUp.this, MainActivity.class);
+                Intent backIntent = new Intent(SignUp.this, MainActivity.class);
                 startActivity(backIntent);
                 break;
             case R.id.sumbit_button:
-                firstName=findViewById(R.id.firstNameEditText);
-                middleName=findViewById(R.id.middlename_EditText);
-                lastName=findViewById(R.id.lastname_EditText);
-                emailId=findViewById(R.id.email_EditText);
-                Intent signupIntent=new Intent(SignUp.this, LoginConformation.class);
-                String firstname=firstName.getText().toString();
-                signupIntent.putExtra(Constants.firstnameKey,firstname );
-               signupIntent.putExtra(Constants.middlenameKey, middleName.getText().toString());
+
+
+                Intent signupIntent = new Intent(SignUp.this, LoginConformation.class);
+
+                signupIntent.putExtra(Constants.firstnameKey, firstName.getText().toString());
+                signupIntent.putExtra(Constants.middlenameKey, middleName.getText().toString());
                 signupIntent.putExtra(Constants.lastnameKey, lastName.getText().toString());
                 signupIntent.putExtra(Constants.emailidKey, emailId.getText().toString());
                 startActivity(signupIntent);
                 break;
-            case R.id.firstNameEditText:
 
-                break;
-            case R.id.middlename_EditText:
-
-                break;
-            case R.id.lastname_EditText:
-                                break;
-            case R.id.email_EditText:
-
-                break;
         }
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean isFocused) {
+                if (isFocused)
+                    Toast.makeText(this, "Focused", Toast.LENGTH_SHORT).show();
+                else if (!isFocused)
+                    Toast.makeText(this, "Lost Focus", Toast.LENGTH_SHORT).show();
+
     }
 }
